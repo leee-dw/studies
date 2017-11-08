61781ce733dc4a45a59cc1915d5020fb4366586c
likedemian <likedemian@me.com>
2017-11-08 21:48:06 +0900

    flex-box in react-native app

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ActivytyIndicator } from 'react-native';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ActivytyIndicator />
-        <Text>Hello! I'm a Native Developer Now! And I hate React</Text>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
       </View>
     );
   }
@@ -16,7 +42,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  redView: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
+  },
+  yellowView: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'yellow',
   },
 });
