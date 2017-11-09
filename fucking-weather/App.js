2886ac2dc197f5c2e49da8cfe0cb54c4c8f62ec9
likedemian <likedemian@me.com>
2017-11-09 13:30:53 +0900

    added fucking weather app with location

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
+import Weather from './Weather';
 
-export default class App extends React.Component {
+
+
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        isLoaded: true
+      });
+    },
+    error => {
+      console.log(error);
+    }
+  );
+  }
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -42,19 +40,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    flexDirection: 'row',
-    flexWrap: 'wrap'
   },
-  redView: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'red'
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
   },
-  yellowView: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'yellow',
-  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 24
+
+
+  }
 });
