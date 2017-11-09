246040bc5c25162ee80cd0386e1a8bab67096586
likedemian <likedemian@me.com>
2017-11-09 13:53:10 +0900

    get location Weather API

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 import Weather from './Weather';
 
-
+const API_KEY ="14e26096f779eb283e0c7b5f3c9715e9";
 
 export default class App extends Component {
   state = {
     isLoaded: false,
-    error: null
+    error: null,
+    temperature: null,
+    name: null
   };
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.setState({
-        isLoaded: true
-      });
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
     },
     error => {
       this.setState({
         error: error
-      })
-
+      });
     }
   );
+  }
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature: json.main.temp, 
+        name: json.weather[0].main
+      })
+    })
   }
   render() {
     const { isLoaded, error } = this.state;
