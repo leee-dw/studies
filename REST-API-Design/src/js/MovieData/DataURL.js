f6bbdd0071df769b9f54eecb29a74a7ce4e63e4d
Dongwoo Lee <likedemian@me.com>
2018-07-26 21:10:45 +0900

    fix: rest api features

--- REST-API-Design/src/js/MovieData/DataURL.js
+++ REST-API-Design/src/js/MovieData/DataURL.js
@@ -3,18 +3,21 @@ export class MovieData {
   constructor() {
     this.url = 'https://api.themoviedb.org/3/movie/';
     this.key = '?api_key=64391ca210dbae0d44b0a622177ef8d3';
+
   }
-  
+
   getNowPlayingURL(language, page) {
-    return `${this.url}now_playing${this.key}&language=${language}&page=${page}`
+    return axios.get(`${this.url}now_playing${this.key}&language=${language}&page=${page}`).then(response => {
+      console.log(response.data)
+    });;
   }
 
   getDetailURL(movieId, language) {
-    return `${this.url + movieId + this.key}&language=${language}`
+    return axios.get(`${this.url + movieId + this.key}&language=${language}`);
   }
 
   getTrailerURL(movieId) {
-    return `${this.url + movieId}/videos${this.key}`;
+    return axios.get(`${this.url + movieId}/videos${this.key}`);
   }
 
 }
--- REST-API-Design/src/js/app.js
+++ REST-API-Design/src/js/app.js
@@ -1,7 +1,37 @@
-import {Controller} from './MovieData/DataController.js'
-import {MovieData} from './MovieData/DataURL.js'
-import {Model} from './MovieData/DataModel.js'
+const url = 'https://api.themoviedb.org/3/movie/';
+const key = '?api_key=64391ca210dbae0d44b0a622177ef8d3';
 
-const dataURL = new MovieData();
-const dataModel = new Model();
-const dataController = new Controller(dataURL, dataModel);
+
+const getNowPlayingURL = (language, page) => {
+  return axios.get(`${url}now_playing${key}&language=${language}&page=${page}`);
+}
+
+const getDetailURL = (movieId, language) => {
+  return axios.get(`${url + movieId + key}&language=${language}`);
+}
+
+const getTrailerURL = (movieId) => {
+  return axios.get(`${url + movieId}/videos${key}`);
+}
+
+
+axios.all([getNowPlayingURL('ko', 1)]).then(axios.spread(response => {
+
+
+  response.data.results.reduce((acc, crr) => {
+
+    axios.all([getDetailURL(crr.id, 'ko'), getTrailerURL(crr.id)])
+
+
+      .then(axios.spread((detail, trailer) => {
+
+        console.log(detail.data, trailer.data.results[0].key);
+
+        
+      }));
+
+
+  }, '')
+}
+
+))
